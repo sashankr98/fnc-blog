117a7bce92b2b3593cd3f35ceb377e9778fd6081
sashankr98 <sashank.r98@gmail.com>
2021-05-30 19:22:00 +0530

    Added first post and removed test post

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -14,10 +14,12 @@ class PostView extends React.Component {
     componentDidMount() {
         const path = `/blog/posts/${this.props.match.params.pid}.md`;
         this.getPost(path)
-            .then(res => this.setState({
-                title: res[0].title,
-                content: res
-            }))
+            .then((res) => {
+                console.log(res);
+                this.setState({
+                    title: res[0].title,
+                    content: res
+            })})
             .catch(err => console.log(err));
     }
 
